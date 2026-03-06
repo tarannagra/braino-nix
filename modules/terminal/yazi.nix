46955e5e3a99f7cbdb3fe977055f6d7c542fe63f
tarannagra <tarannagra@proton.me>
2026-03-06 15:21:08 +0000

    chore: added yazi keymapping for jumping around

--- modules/terminal/yazi.nix
+++ modules/terminal/yazi.nix
@@ -5,6 +5,7 @@
     enable = true;
     settings = {
       yazi = {
+        # mgr
         ratio = [1 4 3];
         sort_by = "natural";
         sort_sensitive = true;
@@ -12,6 +13,15 @@
         show_symlink = true;
         show_hidden = true;
       };
+    };
+    keymap = {
+      input.prepend_keymap = [
+        { on = ["<C-q>"]; run = "close"; }
+      ];
+      mgr.prepend_keymap = [
+        { on = ["g" "d"]; run = "cd ~/docs/"; desc = "Jump to ~/docs"; }
+        # { on = ["g" "D"]; run = "cd "; desc = "";}
+      ];
     };
   };
 }

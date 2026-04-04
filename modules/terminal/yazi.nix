cda33baed3aba62663f72ad9a51d3cd7cc9ae8c7
tarannagra <tarannagra@proton.me>
2026-04-04 17:03:46 +0100

    fix(yazi): properly set config for yazi

--- modules/terminal/yazi.nix
+++ modules/terminal/yazi.nix
@@ -4,23 +4,35 @@
   programs.yazi = {
     enable = true;
     settings = {
-      yazi = {
-        # mgr
+      mgr = {
         ratio = [1 4 3];
         sort_by = "natural";
         sort_sensitive = true;
         sort_reverse = false;
         show_symlink = true;
         show_hidden = true;
+        linemode = "size"; # show file size by default. includes folder count
       };
     };
     keymap = {
       input.prepend_keymap = [
-        { on = ["<C-q>"]; run = "close"; }
+        { on = ["<C-q>"]; run = "close"; desc = "Close input"; }
       ];
       mgr.prepend_keymap = [
+        # forward/backwards directories
+        { on = ["<A-h>"]; run = "back"; desc = "Go backwards a folder"; }
+        { on = ["<A-l>"]; run = "forward"; desc = "Go forwards a folder"; }
+
+        # defaults
         { on = ["g" "d"]; run = "cd ~/docs/"; desc = "Jump to ~/docs"; }
-        # { on = ["g" "D"]; run = "cd "; desc = "";}
+        { on = ["g" "D"]; run = "cd ~/downloads/"; desc = "Jump to ~/downloads"; }
+        { on = ["g" "m"]; run = "cd ~/media/"; desc = "Jump to ~/media"; }
+        { on = ["g" "c"]; run = "cd ~/.config/"; desc = "Jump to ~/.config"; }
+
+        # university
+        { on = ["u" "c"]; run = "cd ~/docs/university_coursework/"; desc = "Jump to university coursework"; }
+        { on = ["u" "w"]; run = "cd ~/docs/university_writeups/"; desc = "Jump to university writeups"; }
+        { on = ["u" "a"]; run = "cd ~/docs/university_archive/"; desc = "Jump to university archive"; }
       ];
     };
   };
